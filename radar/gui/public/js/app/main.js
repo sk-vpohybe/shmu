2438881e7682987af479340fecae8c2d03fc0c63
Peter Vojtek <peter.vojtek@gmail.com>
2016-06-04 18:11:48 +0200

    minor

--- radar/gui/public/js/app/main.js
+++ radar/gui/public/js/app/main.js
@@ -17,7 +17,7 @@ function UserOpts(mainMenu){
    if(localStorage.getItem('mapType'))
     this.mapType = localStorage.getItem('mapType')
    else
-    this.mapType = 'C'
+    this.mapType = 'mapbox'
    
    this.changeMapTypeTo = function(maptype){
        this.mapType = maptype
@@ -87,7 +87,7 @@ function LeafletMap(opts){
   this.map = new L.Map('map', {minZoom: 7, maxZoom: 15});
   this.opts = opts
 
-  this.basemapLayer = new L.TileLayer(this.opts.mapSource, {attribution: '(c) SHMÚ.sk, freemap.sk, openstreetmap.org contributors'});
+  this.basemapLayer = new L.TileLayer(this.opts.mapSource, {attribution: '(c) SHMÚ.sk, freemap.sk, mapbox.com, openstreetmap.org contributors'});
 
   if (this.opts.gpx){
       var lotLan = this.opts.trackToDisplay.geometry.coordinates[0];
